c16ed8e0ab242ef8c8e67a5a158fdec3a83ae193
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-01-13 22:25:27 +0900

    [Silver III] Title: 어린 왕자, Time: 44 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -11,18 +11,16 @@ for _ in range(n):
     s_x, s_y, e_x, e_y = map(int, sys.stdin.readline().split())
     s_cnt = 0
     e_cnt = 0
-    s_e_cnt = 0
+
     m = int(sys.stdin.readline())
     for _ in range(m):
         x, y, r = map(int, sys.stdin.readline().split())
         if dist(s_x, x, s_y, y) < r and dist(e_x, x, e_y, y) < r:
-            s_cnt += 1
-            e_cnt += 1
-            s_e_cnt += 2
+            pass
         elif dist(s_x, x, s_y, y) < r:
             s_cnt +=1
 
         elif dist(e_x, x, e_y, y) < r:
             e_cnt += 1
 
-    print(s_cnt + e_cnt - s_e_cnt)
+    print(s_cnt + e_cnt)
