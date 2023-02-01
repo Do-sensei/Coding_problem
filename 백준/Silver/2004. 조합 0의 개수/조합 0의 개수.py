aefab9e95b9a2c9f2100ee1855faa1f10c295b2f
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-02-01 10:54:52 +0900

    [Silver II] Title: 조합 0의 개수, Time: 44 ms, Memory: 33508 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -0,0 +1,18 @@
+from math import factorial as f
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def cnt_num(n, num):
+    cnt = 0
+    while n:
+        n //= num
+        cnt += n
+    return cnt
+
+cnt_5 = cnt_num(n, 5) - cnt_num(m, 5) - cnt_num(n - m, 5)
+cnt_2 = cnt_num(n, 2) - cnt_num(m, 2) - cnt_num(n - m, 2)
+
+print(min(cnt_5, cnt_2))
