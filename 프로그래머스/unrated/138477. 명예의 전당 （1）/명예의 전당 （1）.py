4ad24e1c9dcd72b7df5bf2e5533e91df29a42f73
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-02-07 13:47:23 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,12 +2,10 @@ def solution(k, score):
     answer = []
     arr = []
     for i in range(len(score)):
+        arr.append(score[i])
+        arr.sort(reverse=True)
         if i < k:
-            arr.append(score[i])
-            arr.sort(reverse=True)
             answer.append(arr[-1])
         else:
-            arr.append(score[i])
-            arr.sort(reverse=True)
             answer.append(arr[k-1])
     return answer
