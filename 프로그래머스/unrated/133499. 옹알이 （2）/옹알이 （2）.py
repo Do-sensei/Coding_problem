bae5f5afbaed470b6873e848444e40e0d9420d45
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-02-15 13:10:25 +0900

    [unrated] Title: 옹알이 (2), Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+def solution(babbling):
+    answer = 0
+    babb_dict = {"aya":1, "ye":2, "woo":3, "ma":4}
+    word_arr = []
+    sub_cnt = 0
+    for i in babbling:
+        a = i
+        for j in babb_dict.items():
+            a = a.replace(j[0], str(j[1]))
+        if a.isdecimal():
+            word_arr.append(a)
+    for i in word_arr:
+        for j in range(len(i)-1):
+            if i[j] == i[j+1]:
+                sub_cnt += 1
+                break
+    
+    answer = len(word_arr) - sub_cnt
+    
+    return answer
