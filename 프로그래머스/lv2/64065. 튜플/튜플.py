c422ba7fed842f2a0e46dc4860a74c6fd6ee727a
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-08-24 20:40:20 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,12 +1,25 @@
+# def solution(s):
+#     s_str_list = s[2:-2].split('},{')
+#     for k, v in enumerate(s_str_list):
+#         s_str_list[k] = list(map(int, v.split(',')))
+#     s_dic = {len(v):v for v in s_str_list}
+#     sort_s = sorted(s_dic.items(), key=lambda x: x[0], reverse=False)
+#     result = []
+#     for i in sort_s:
+#         for j in i[1]:
+#             if j not in result:
+#                 result.append(j)
+#     return result
+
 def solution(s):
-    s_str_list = s[2:-2].split('},{')
-    for k, v in enumerate(s_str_list):
-        s_str_list[k] = list(map(int, v.split(',')))
-    s_dic = {len(v):v for v in s_str_list}
-    sort_s = sorted(s_dic.items(), key=lambda x: x[0], reverse=False)
+    s_list = [list(map(int, item.split(','))) for item in s[2:-2].split('},{')]
+    
+    s_list.sort(key=len)
+    
     result = []
-    for i in sort_s:
-        for j in i[1]:
-            if j not in result:
-                result.append(j)
+    for sublist in s_list:
+        for num in sublist:
+            if num not in result:
+                result.append(num)
+                
     return result
