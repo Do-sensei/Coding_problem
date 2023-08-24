5784b7aeb746914352021d6db7b9753f9d230f34
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-08-24 20:56:36 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,40 @@
+# def solution(operations):
+#     answer = []
+
+#     for i in operations:
+#         if i[0] == 'I':
+#             answer.append(int(i[2:]))
+#         elif i[0] == 'D':
+#             if i[2:] == '1':
+#                 try:
+#                     answer.remove(max(answer))
+#                 except:
+#                     pass
+#             elif i[2:] == '-1':
+#                 try:
+#                     answer.remove(min(answer))
+#                 except:
+#                     pass
+
+#     if len(answer) == 0:
+#         answer = [0, 0]
+#     else:
+#         answer = [max(answer), min(answer)]
+
+#     return answer
+
 def solution(operations):
     answer = []
 
-    for i in operations:
-        if i[0] == 'I':
-            answer.append(int(i[2:]))
-        elif i[0] == 'D':
-            if i[2:] == '1':
-                try:
-                    answer.remove(max(answer))
-                except:
-                    pass
-            elif i[2:] == '-1':
-                try:
-                    answer.remove(min(answer))
-                except:
-                    pass
+    for op in operations:
+        command, value = op.split()
+        value = int(value)
 
-    if len(answer) == 0:
-        answer = [0, 0]
-    else:
-        answer = [max(answer), min(answer)]
+        if command == 'I':
+            answer.append(value)
+        elif command == 'D' and value == 1 and answer:
+            answer.remove(max(answer))
+        elif command == 'D' and value == -1 and answer:
+            answer.remove(min(answer))
 
-    return answer
+    return [max(answer), min(answer)] if answer else [0, 0]
