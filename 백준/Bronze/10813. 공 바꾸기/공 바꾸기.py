fc957bae15402c3202d3920594c6846cc077de7b
do_sensei_ <92496115+Do-sensei@users.noreply.github.com>
2023-08-25 15:23:02 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+n_ls = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    n_ls[i-1], n_ls[j-1] = n_ls[j-1], n_ls[i-1]
+
+print(*n_ls)
